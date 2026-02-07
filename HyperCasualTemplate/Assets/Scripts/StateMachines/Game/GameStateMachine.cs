4400ddec4ca40936e989e9c10a80d9ba3e16bf26
Trần Việt Anh <63540274+tranvietanh0@users.noreply.github.com>
2026-02-07 11:01:42 +0700

    Initial commit

---
+++ HyperCasualTemplate/Assets/Scripts/StateMachines/Game/GameStateMachine.cs
@@ -0,0 +1,24 @@
+﻿namespace HyperCasualGame.Scripts.StateMachines.Game
+{
+    using System.Collections.Generic;
+    using GameFoundationCore.Scripts.DI;
+    using GameFoundationCore.Scripts.Signals;
+    using HyperCasualGame.Scripts.StateMachines.Game.States;
+    using UITemplate.Scripts.Others.StateMachine.Controller;
+    using UITemplate.Scripts.Others.StateMachine.Interface;
+    using UniT.Logging;
+
+    public class GameStateMachine : StateMachine, IInitializable
+    {
+        public GameStateMachine(
+            List<IState>   listState,
+            ILoggerManager loggerManager,
+            SignalBus      signalBus
+        ) : base(listState, loggerManager, signalBus) { }
+
+        public void Initialize()
+        {
+            this.TransitionTo<GameHomeState>();
+        }
+    }
+}
